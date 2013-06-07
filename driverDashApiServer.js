da52058896d8738bd800b3d389b6463767e9dfbe
OpenShift guest <abrar.here@gmail.com>
2013-06-07 00:37:22 -0400

    c

--- driverDashApiServer.js
+++ driverDashApiServer.js
@@ -1,1 +1,13 @@
 //node js main se4rver file
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
