c093df403d16fbb134ad46d8b73ed31d75126f18
Abrar <ahmeda8@outlook.com>
2013-06-07 15:19:39 -0700

    A

--- driverDashApiServer.js
+++ driverDashApiServer.js
@@ -1,10 +1,11 @@
 //node js main se4rver file
 var express = require("express");
+var util = require("util");
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!'+process.env);
+  response.send('Hello World!'+util.inspect(process.env));
 });
 
 var port = process.env.PORT;
