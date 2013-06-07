cd9e6cfaa762ef761fc8575e8c7173b86d8dac83
Abrar <ahmeda8@outlook.com>
2013-06-07 15:26:10 -0700

    A

--- driverDashApiServer.js
+++ driverDashApiServer.js
@@ -1,10 +1,22 @@
 //node js main se4rver file
 var express = require("express");
 var util = require("util");
+var pg = require('pg');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
+
+
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  var query = client.query('SELECT * FROM your_table');
+
+  query.on('row', function(row) {
+    console.log(JSON.stringify(row));
+  });
+});
+
   response.send('Hello World!'+util.inspect(process.env));
 });
 
