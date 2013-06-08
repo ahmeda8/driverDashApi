efaa33eec2b29129664cbbeb0830bcef9907b7d8
OpenShift guest <abrar.here@gmail.com>
2013-06-08 04:34:49 -0400

    C

--- savefile.js
+++ savefile.js
@@ -3,5 +3,8 @@ var fs = require("fs");
 
 exports.directoryExists = function(directory,callback)
 {
-    var exists = fs.exists(directory,callback);
+    var exists = fs.exists(directory,function(exists,callback){
+        fs.mkdir(directory,callback);
+    });
 }
+
