bbf99b08fed3f70f06343f3bdd73347d0967e0e4
OpenShift guest <abrar.here@gmail.com>
2013-06-07 20:03:31 -0400

    C

--- driverDashApiServer.js
+++ driverDashApiServer.js
@@ -9,11 +9,11 @@ app.get('/', function(request, response) {
   response.send('Driver DASH API Root');
 });
 
-app.get('/user', function(request, response) {
+app.get('/user/:id/:fbid', function(req, response) {
   var ui = require("./userinfo.js");
   var name = ui.getName(1);
   //var name = util.inspect(ui);
-  response.send('Driver DASH API User'+name);
+  response.send('Driver DASH API User'+name+"---"+req.params.id+"---"+req.params.fbid);
 });
 
 app.get('/backup', function(request, response) {
