d1cf7625545aa23bd65e79e105a9c47a5ebfd9a5
Abrar Ahmed <ahmeda8@outlook.com>
2013-06-11 20:51:12 -0700

    adding_pg_sql_db

--- userinfo.js
+++ userinfo.js
@@ -3,7 +3,7 @@ var pg = require("pg");
 
 exports.createUser = function(userObj,callback)
 {
-	console.log(userObj);
+	//console.log(userObj);
 	var client = new pg.Client(process.env.HEROKU_POSTGRESQL_SILVER_URL);
 	var sql = {
 			text:"INSERT INTO users (fb_id,email,created,last_login,first_name,last_name) values ($1,$2,$3,$4,$5,$6)",
@@ -11,9 +11,10 @@ exports.createUser = function(userObj,callback)
 			};
 	
 	client.connect(function(err){
-		console.log(err);
+		//console.log(err);
 		client.query(sql,function(err,result){
 			client.end();
+			callback();
 		});
 		});
 }
