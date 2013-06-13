b6a49de654efdd7a0c9aaf7fb159b0c0848e1e4c
Abrar <ahmeda8@outlook.com>
2013-06-12 17:32:36 -0700

    cleaning up api layout

--- driverDashApiServer.js
+++ driverDashApiServer.js
@@ -9,17 +9,31 @@ app.use(express.bodyParser());
 if(!process.env.HEROKU_POSTGRESQL_SILVER_URL)
 		process.env.HEROKU_POSTGRESQL_SILVER_URL = "postgres://ydgammzyfnciqp:8Z0VP8iiaO9qeWrmpQYROn0fpc@ec2-107-20-215-249.compute-1.amazonaws.com:5432/d9d08kbuji2has";
 
+/*POST API*/
 app.post('/user',function(req,res)
 {
-	
 	var userObj = JSON.parse(req.body.user);
 	var ui = require("./userinfo.js");
-	
 	ui.createUser(userObj,function(err,result)
 	{
 		res.send(util.inspect(err));
 	});
 });
+
+app.post('/backup', function(request, response) {
+	
+	var info = JSON.parse(request.body.info);
+	console.log(info);
+	var ui = require("./userinfo.js");
+	ui.addBackupFile(info,function(err,result)
+	{
+		response.send(util.inspect(result));
+	});
+});
+
+/*END POST API*/
+
+/*START GET API*/
 
 app.get('/user/:fbid',function(req,res) {
 	var ui = require("./userinfo.js");
@@ -34,20 +48,9 @@ app.get('/user/backups/:iduser', function(req, response) {
 	  response.send(util.inspect(data.rows));
   });
 });
+/*end get api*/
 
-app.post('/backup', function(request, response) {
-	
-	var info = JSON.parse(request.body.info);
-	console.log(info);
-	var ui = require("./userinfo.js");
-	ui.addBackupFile(info,function(err,result)
-	{
-		response.send(util.inspect(result));
-	});
-	//response.send("ok");
-});
-
-
+/*SERVER*/
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
