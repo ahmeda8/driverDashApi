a9667bb1ddc8924be3b7c399cf50aca0211ecf07
Abrar <ahmeda8@outlook.com>
2013-06-12 22:51:37 -0700

    add user now send the row back

--- userinfo.js
+++ userinfo.js
@@ -8,7 +8,13 @@ exports.addUser = function(userObj,callback)
 			text:"INSERT INTO users (fb_id,email,created,last_login,first_name,last_name) values ($1,$2,$3,$4,$5,$6)",
 			values:[userObj.fbid,userObj.email,userObj.created,userObj.last_login,userObj.firstname,userObj.lastname]
 		};
-	query(sql,callback);
+	query(sql,function(err,result){
+		var sql1 = {
+			text:"SELECT * from users WHERE fb_id =$1",
+			values:[userObj.fbid]
+		};
+		query(sql1,callback);
+	});
 	
 };
 
