4e88e3ded1f46221baf80e03283f04e4af7ff215
Abrar <ahmeda8@outlook.com>
2013-07-25 05:17:35 -0700

    using https

--- userinfo.js
+++ userinfo.js
@@ -57,9 +57,10 @@ exports.deleteBackup = function(id,callback)
 		method:'DELETE',
 		//port:80,
 		headers:{
-			//'Content-Length':res.rows[0].download_url.length,
+			'Content-Length':res.rows[0].download_url.length,
 			'Connection':'keep-alive',
-			'Content-Type':'text/plain'
+			'Content-Type':'text/plain',
+			'User-Agent':'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0'
 		}
 	 };
 	 console.log(options);
